49f4d3b9075bf6e793984e6ee989272a4d34285b
Adam Murray <a.murray0413@gmail.com>
2024-10-29 15:43:31 -0700

    Update __init__.py

--- peplab/core/reaction/__init__.py
+++ peplab/core/reaction/__init__.py
@@ -1,5 +1,8 @@
 from .reactive_site import ReactiveSite
-from .reaction_mechanism import ReactionMechanism
+# from .reaction_mechanism import ReactionMechanism
 from .reaction_pathway import ReactionPathway
 
-__all__ = ['ReactiveSite', 'ReactionMechanism', 'ReactionPathway']
+__all__ = [
+    'ReactiveSite',
+#     'ReactionMechanism',
+    'ReactionPathway']
