708c14ee3788831cbfe60d2c17a6037cf0eb44f2
Adam Murray <a.murray0413@gmail.com>
2024-10-29 15:46:26 -0700

    fix

--- peplab/analysis/property/alogp_calculator.py
+++ peplab/analysis/property/alogp_calculator.py
@@ -1,7 +1,7 @@
 from rdkit import Chem
 from rdkit.Chem import Descriptors
 
-from property_calculator import PropertyCalculator
+from .property_calculator import PropertyCalculator
 
 class ALogPCalculator(PropertyCalculator):
     """Calculates atomic logP."""
--- peplab/analysis/property/rotatable_bonds_calculator.py
+++ peplab/analysis/property/rotatable_bonds_calculator.py
@@ -1,7 +1,7 @@
 from rdkit import Chem
 from rdkit.Chem import Descriptors
 
-from property_calculator import PropertyCalculator
+from .property_calculator import PropertyCalculator
 
 class RotatableBondsCalculator(PropertyCalculator):
     """Counts rotatable bonds."""
