7662b802a8223fd7f1be779a313647ee787d38b5
Adam Murray <a.murray0413@gmail.com>
2024-10-29 15:35:54 -0700

    fix

--- peplab/analysis/__init__.py
+++ peplab/analysis/__init__.py
@@ -1,4 +1,5 @@
-from .property import ThermodynamicParameters, ReactivityProfile
-from .structure import StructureGenerator
+from .property.thermodynamics import ThermodynamicParameters
+from .property.reactivity_profile import ReactivityProfile
+from .structure.structure_generator import StructureGenerator
 
 __all__ = ['ThermodynamicParameters', 'ReactivityProfile', 'StructureGenerator']
---
+++ peplab/analysis/structure/structure_generator.py
@@ -0,0 +1,7 @@
+
+class StructureGenerator:
+    def __init__(self, structure):
+        self.structure = structure
+
+    def generate(self):
+        pass
