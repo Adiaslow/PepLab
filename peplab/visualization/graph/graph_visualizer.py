111cf69310d51963dc856708316c28bab937ef9e
Adam Murray <a.murray0413@gmail.com>
2024-10-30 08:06:50 -0700

    Update graph_visualizer.py

--- peplab/visualization/graph/graph_visualizer.py
+++ peplab/visualization/graph/graph_visualizer.py
@@ -2,6 +2,8 @@ from typing import Tuple, Optional
 import matplotlib.pyplot as plt
 import networkx as nx
 
+from ..cpk_colors import CPKColors
+
 
 class GraphVisualizer:
     """Visualizes molecular graphs using NetworkX."""
